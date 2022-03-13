8776d71b4bcfeccae35022637bae5f8db2b94261
september21st <tsuilook@gmail.com>
2022-03-13 23:42:58 +0900

    :tada: init project

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>next js blog template by borachoi.dev</title>
+      </Head>
+      <h2>Hello</h2>
+    </div>
+  )
+}
+
+export default Home
