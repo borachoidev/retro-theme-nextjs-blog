c8e67b172971e8bdcf529f864948751746d2693e
september21st <tsuilook@gmail.com>
2022-03-18 20:44:54 +0900

    :bug: tag 컴포넌트 스타일 오류 수정

--- src/components/tag.tsx
+++ src/components/tag.tsx
@@ -2,9 +2,16 @@ import styles from '../styles/tag.module.css'
 
 interface TagProps {
   text: string
+  active?: boolean
 }
-const Tag = ({ text }: TagProps) => {
-  return <div className={styles.tag}>{text}</div>
+const Tag = ({ text, active = false }: TagProps) => {
+  const tag = text.replace(/\s+/g, '-').toUpperCase()
+
+  return (
+    <div className={[styles.tag, active ? styles.active : ''].join(' ')}>
+      {tag}
+    </div>
+  )
 }
 
 export default Tag
