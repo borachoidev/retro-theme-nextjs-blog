f8eb0bbb35d04c57e969b6297fea948ba0eaf948
september21st <tsuilook@gmail.com>
2022-03-18 21:30:17 +0900

    :wrench: 절대경로 설정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
+const path = require('path')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack(config, { webpack }) {
+    config.resolve = {
+      alias: {
+        '@utils': path.resolve(__dirname, 'src/utils'),
+        '@styles': path.resolve(__dirname, 'src/styles'),
+        '@components': path.resolve(__dirname, 'src/components'),
+      },
+      ...config.resolve,
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
