5eca99523c8d309995b1a40b9c6659db984acb8c
september21st <tsuilook@gmail.com>
2022-03-18 20:14:45 +0900

    :lipstick: ui 스타일 변경 + GNB 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,11 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Header from '../components/header'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Header></Header>
       <Component {...pageProps} />
     </>
   )
