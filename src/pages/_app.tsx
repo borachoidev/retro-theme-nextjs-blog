134834b001bee7d575ce0056c09d06d52eab4bf5
september21st <tsuilook@gmail.com>
2022-03-21 15:41:05 +0900

    :lipstick: dark theme 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,14 @@
 import Header from '@components/header'
 import '@styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'next-themes'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider>
       <Header></Header>
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   )
 }
 
