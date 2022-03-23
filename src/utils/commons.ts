f0487a02515dfe309594030c415e970b6d25eb1d
september21st <tsuilook@gmail.com>
2022-03-23 19:08:00 +0900

    :sparkles: 케밥케이스 util 추가

--- src/utils/commons.ts
+++ src/utils/commons.ts
@@ -1,5 +1,12 @@
+import slug from 'github-slugger'
+const { slug: slugger } = slug
+
 export function dateSortDesc(a: string, b: string) {
   if (a > b) return -1
   if (a < b) return 1
   return 0
+}
+
+export const kebabCase = (text: string, uppercase?: boolean) => {
+  return uppercase ? slugger(text).toUpperCase() : slugger(text)
 }
